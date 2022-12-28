6512a6c37505dd234db0b00451835ba0147587f0
Maria Emilia Castro Arguello <milicastroarguello@hotmail.com>
2022-12-28 18:53:24 -0300

    preentrega3

--- index.js
+++ index.js
@@ -1,93 +1,109 @@
-const ropa = [
-    {id:1, nombre: 'Remera Agatha', temporada: 'invierno', precio: 1000, oferta: false, descripcion: 'Remera de algodon color verde'},
-    {id:2, nombre: 'Remera Roma', temporada: 'sale', precio: 800, oferta: true, descripcion: 'Remera de algodon color rosa'},
-    {id:3, nombre: 'Pantalón Cala', temporada: 'verano', precio: 1500, oferta: false, descripcion: 'panatalón color verde'},
-    {id:4, nombre: 'Pantalón Fermín', temporada: 'sale', precio: 1200, oferta: true, descripcion: ' pantalon de gabardina color terracota'},
-    {id:5, nombre: 'Pollera Ana', temporada: 'verano', precio: 2000, oferta: false, descripcion: 'pollera de jean oscuro'},
-    {id:6, nombre: 'Short Rufino', temporada: 'verano', precio: 750, oferta: false, descripcion: 'short tiro alto color negro'},
-    {id:7, nombre: 'Pantalón Tiffany', temporada: 'sale', precio: 1200, oferta: true, descripcion: 'pantalon corte cintura color violeta'},
-    {id:8, nombre: 'Pollera Alison', temporada: 'verano', precio: 2500, oferta: false, descricpion: 'pollera pantalon color amarillo'},
-    {id:9, nombre: 'Short Rosi', temporada: 'verano', precio: 750, oferta: false, descricpion: 'short de jean'},
-    {id:10,nombre: 'Remera Caro', temporada: 'sale', precio: 1090, oferta: true, descripcion: 'remera de jersey negra'},
-    {id:11,nombre: 'Remera Bertina', temporada: 'verano', precio: 900, oferta: true, descripcion: 'remera escote V color crema'},
-    {id:12,nombre: 'Top Manhattan', temporada: 'invierno', precio: 1500, oferta: true, descricpion: 'Top una sola manga color negro y blanco'},
-    {id:13,nombre: 'Top Noche', temporada: 'sale', precio: 1800, oferta: true, descripcion: 'Top cortito metalizado'},
-    {id:14,nombre: 'Vestido Noche', temporada: 'verano', precio: 10000, oferta: false, descripcion: 'Vestido largo, corte princesa'},
-    {id:15,nombre: 'Vestido Día', temporada: 'verano', precio: 5000, oferta: false, descripcion: 'vestido suelto color beige'},
-
+const stockRopa = [
+    {id: 1, nombre: 'Remera Mezcla', tipo: 'Remera', talle: 1, precio: 1000, img: "remeraMezcla.webp" },
+    {id: 2, nombre: 'Remera Port', tipo: 'Remera', talle: 2 , precio: 800, img: "remeraPort.webp"},
+    {id: 3, nombre: 'Remera Satelital',tipo: 'Remera',  talle: 2, precio: 1500, img: "remeraSatelital.webp"},
+    {id: 4, nombre: 'Remera Senderos',tipo: 'Remera',  talle: 3 , precio: 1200, img: "remeraSenderos.webp"},
+    {id: 5, nombre: 'Top Arrecife', tipo: 'Remera Top', talle: 2, precio: 2000, img: "topArrecife.webp"},
+    {id: 6, nombre: 'Jean Oscuro',tipo: 'Denim Jean',  talle: 2, precio: 1750, img: "jeanoscuro.webp"},
+    {id: 7, nombre: 'Pantalón Uno',tipo: 'Denim Jean',  talle: 1, precio: 2000, img: "pantalonuno.webp"},
+    {id: 8, nombre: 'Pantalón Tres', tipo: 'Denim Jean', talle: 1, precio: 550, img: "pantalonTres.webp"},
+    {id: 9, nombre: 'Vestido Napo', tipo: 'Vestido', talle: 1, precio: 1550, img: "vestidoNapo.webp"},
+    {id: 10, nombre: 'Vestido Carpaccio',tipo: 'Vestido',  talle: 2, precio: 1850, img: "vestidoCarpaccio.webp"},
+    {id: 11, nombre: 'Pollera Galup',tipo: 'Pollera',  talle: 1, precio: 4500, img: "polleraGalupe.webp"},
+    {id: 12, nombre: 'Pollera Pop',tipo: 'Pollera',  talle: 5, precio: 1500, img: "polleraPop.webp"},
+    {id: 13, nombre: 'Pollera Ciao', tipo: 'Pollera', talle: 5, precio: 4000, img: "polleraCiao.webp"},
 ];
-let mensaje = "Estos son los productos disponibles para su seleccion:\n";
-let resultado = [];
-let limite_precio;
-let temporada;
-comenzar();
 
-function comenzar(){
-    temporada = prompt("Seleccione una temporada entre Invierno-Verano-Sale");
-    evaluar_temporada(temporada);
-}
+let carrito = localStorage.getItem("storageCarrito") ? JSON.parse(localStorage.getItem("storageCarrito")) : []
+let iconoCarrito = document.getElementById("carritoContenedor");
+let listaCarrito = document.getElementById("listaCarrito")
+let totalCarrito = 0;
+iconoCarrito.innerHTML=carrito.length;
 
+const contenedor = document.querySelector('#contenedor')
+stockRopa.forEach((prendas) => {
+    //console.log(prendas);
+    const {id, nombre, tipo,talle, precio, img} = prendas
+    //console.log(nombre);
+    contenedor.innerHTML += `
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+  <img src="multimedia/${img}" class="card-img-top mt-2" alt="imagen de prenda">
+  <div class="card-body">
+    <h5 class="card-title">${nombre}</h5>
+    <p class="card-text">Precio: $${precio}</p>
+    <p class="card-text">Tipo: ${tipo}</p>
+    
+    <button data-id="${id}" class="btn btn-primary agregar">Agregar al carrito</button>
+  </div>
+</div>
+</div>
+    `
+});
 
-function evaluar_temporada(temporada){
-    switch(temporada.toLowerCase()){
-        case "invierno":
-        case "verano":    
-        case "sale":
-                limite_precio = prompt("Seleccione un limite de precio");
-                evaluar_precio(limite_precio);
-                //console.log(typeof limite_precio)
-                
-                for (let i = 0; i < ropa.length; i++) {
-                    //console.log(ropa[i]);
-                    if(ropa[i].temporada == temporada.toLowerCase() && ropa[i].precio <= parseInt(limite_precio)) {
-                        mensaje += ropa[i].nombre + ": $" + ropa[i].precio + "\n";
-                        resultado.push(ropa[i]);    
-                    }
-                }      
-                if(resultado.length) {
-                    if(confirm(mensaje + "\n ¿Le interesaría ver nuestras ofertas?")) {
-                    let ofertas = ropa.filter (( {oferta}) => oferta);
-                    //console.log(ofertas);
-                    let resultado_ofertas = " ";
-                    ofertas.forEach(oferta => {
-                        resultado_ofertas += oferta.nombre + " " + oferta.temporada + " : $ " + oferta.precio + " \n";
-                    });
+let botones = document.querySelectorAll(".agregar");
 
-                    let busqueda = prompt (resultado_ofertas + "\n o sino ingrese el nombre del producto deseado");
-                    let producto_buscado = ropa.find (({nombre}) => nombre.toLocaleLowerCase () === busqueda.toLocaleLowerCase());
-                 //console.log(productos_buscado);
+//console.log(botones);
+botones.forEach((boton) => {
+    boton.addEventListener("click",() => {
+        const item = stockRopa.find((prendas) => prendas.id === parseInt(boton.dataset.id))
+        //console.log(item)
+        //console.log(boton.dataset.id)
+        carrito.push(item);
+        //console.log(carrito);
 
-                if (producto_buscado){
-                    alert(`Resultado de la búsqueda: \n ${producto_buscado.nombre} ${producto_buscado.temporada}: $${producto_buscado.precio}`)
-                }else {
-                    alert (`Ese producto ya no esta disponible`)
-                }
+        iconoCarrito.innerHTML= carrito.length;
+        localStorage.setItem("storageCarrito", JSON.stringify(carrito))
+    })
+});
+
+iconoCarrito.addEventListener("click", () => {
+    listaCarrito.innerHTML="";
+    carrito.forEach((prenda) => {
+        //console.log(prendas);
+        const { id, nombre, talle, precio, img } = prenda
+        //console.log(nombre);
+        listaCarrito.innerHTML += `
+        <li>
+      <img class="img-carrito" src="multimedia/${img}" alt="imagen de prendas">
+        <p> ${nombre} Precio: $${precio}</p>
+        <a class="eliminar" data-id="${id}" data-precio="${precio}"> x </a>
+        </li> 
+        `
+        totalCarrito += parseInt(precio);
+    })
+    listaCarrito.innerHTML += `<li id="totalLista">Total: $${totalCarrito}</li>`;
+    let botonesEliminar= document.querySelectorAll(".eliminar");
+    botonesEliminar.forEach((boton) => {
+        boton.addEventListener("click", () => {
+            const item = carrito.find((prendas) => prendas.id === parseInt(boton.dataset.id));
+            const index= carrito.indexOf(item);
+            if(index > -1){
+                carrito.splice(index, 1);
             }
+            totalCarrito -= parseInt(boton.dataset.precio)
+            document.getElementById("totalLista").innerHTML = `Total: $${totalCarrito}`;
+            localStorage.removeItem("storageCarrito");
+            localStorage.setItem("storageCarrito", JSON.stringify(carrito));
+            boton.parentElement.remove();
+            iconoCarrito.innerHTML=carrito.length;
 
-                } else {
-                    alert(`No encontramos ninguna seleccion para ese pedido. Por favor vuelva a comenzar`);
-                    comenzar();
-                }
-            break;  
-        default:
-            let nueva_temporada = prompt("Error- Por favor Seleccione una temporada entre Inierno-Verano-Sale");
-            evaluar_temporada(nueva_temporada);
-            break;
-    }  
-}
+        })
+    })
+    
 
-function evaluar_precio(precio){
-    let nuevo_precio;
-    if(isNaN(precio)){
-        nuevo_precio = prompt("Error- Por favor seleccione un valor numérico");
-        evaluar_precio(nuevo_precio);
-        return; 
-    }
-
-}
-let ropaOrdenada = [...ropa].sort((a, b) => {
-    return a - b
 })
 
-console.log(ropaOrdenada)
+// const botonModos = document.querySelector("#claro-oscuro")
+// const body = document.querySelector (".modo-claro")
+
+// botonModos.onclick = () => {
+//     body.classList.toggle("modo-oscuro")
+//     if (body.className === "modo-claro modo-oscuro")
+//         { botonModos.textContent = "Modo Claro"}
+//         else{
+//             botonModos.textContent = "Modo Oscuro"
+//             }
+// }
+
 
